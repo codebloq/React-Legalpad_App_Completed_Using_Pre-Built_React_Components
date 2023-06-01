39b6a85da92ae90a03743707bde6dd405dc9d3dd
The Advocate <134937174+lawyerWho@users.noreply.github.com>
2023-06-01 19:57:52 +0530

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Gavel from "@material-ui/icons/Gavel";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <Gavel />
+        Legalpad
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
